9e02ee0dc260a9c30763ee8aa282672d84376c2a
Максим Ю <66893689+smorfmorf@users.noreply.github.com>
2026-04-02 08:44:42 +1000

    00

--- database.py
+++ database.py
@@ -3,6 +3,13 @@ import asyncio
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
+
+# Загружаем переменные из .env
+load_dotenv()  # по умолчанию ищет .env в текущей папке
+MY_ENV = os.getenv("MY_VAR")
+print(MY_ENV)
+
 
 class BaseORM(DeclarativeBase):
     pass
@@ -13,15 +20,16 @@ async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False) # 
 
 
 async def main():
+    # Низкий уровень: чистый SQL
     # async with engine.begin() as conn:  # ← транзакция открывается
     #    res =  await conn.execute(text("SELECT * FROM reports"))  # ← выполняем запрос
     #    print( res.fetchall())  # ← 1, так как была вставлена одна строка
 
+    # Пример 1: сессия для выполнения ORM запросов 
     async with async_session_maker() as session:
-        async with session.begin():  # ← транзакция
-            result = await session.execute(text("SELECT * FROM reports"))
-            rows = result.fetchall()
-            print(rows)
+        result = await session.execute(text("SELECT * FROM reports"))
+        rows = result.fetchall()
+        print(rows)
 
 if __name__ == "__main__":
     asyncio.run(main())
