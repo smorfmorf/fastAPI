fdd75134e9b8566cc36950020ce82eb5ddaf6a5c
Максим Ю <66893689+smorfmorf@users.noreply.github.com>
2026-04-02 08:48:08 +1000

    Create rooms.py

---
+++ models/rooms.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+
+from databese import Base_ORM
+
+
+class RoomsOrm(Base_ORM):
+    __tablename__ = "rooms"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id"))
+    title: Mapped[str]
+    description: Mapped[str | None]
+    price: Mapped[int]
+    quantity: Mapped[int]
